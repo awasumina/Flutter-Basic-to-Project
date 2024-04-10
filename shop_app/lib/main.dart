7379a129e17db828529ac9e2fde6ef432ce70069
sumina <suminaawa123@gmail.com>
2024-04-10 19:07:55 +0545

    Shoes Shop App

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title : 'Shopping App',
+      title : 'Shopping App', 
       theme: ThemeData(
           fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSeed(
