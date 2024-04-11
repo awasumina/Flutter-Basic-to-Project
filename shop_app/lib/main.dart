12739bcfc994f359f525ddb08283b543fdcb33d4
sumina <suminaawa123@gmail.com>
2024-04-11 19:28:18 +0545

    Shoes Shop App

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  @override
+  @override 
   Widget build(BuildContext context) {
     return MaterialApp(
       title : 'Shopping App',
