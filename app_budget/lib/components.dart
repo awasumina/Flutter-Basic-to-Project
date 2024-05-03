8f6b2c0a848aa65ff2b112b3e0a02fcbb8221475
sumina <suminaawa123@gmail.com>
2024-05-03 22:04:24 +0545

    Portfolio-web and mobile

--- app_budget/lib/components.dart
+++ app_budget/lib/components.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class OpenSans extends StatelessWidget {
-  final text;
+  final text; 
   final size;
   final color;
   final fontWeight;
